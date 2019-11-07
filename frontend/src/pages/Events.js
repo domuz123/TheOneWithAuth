5f915c7fd78d42eeb16f5c4697fade814fa6e0f3
Ivan <ivanknezevic1405@gmail.com>
2019-11-07 21:33:09 +0100

    modal

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,10 +1,44 @@
-import React from 'react' 
+import React, {Component} from 'react' 
+import './Events.css'
+import Modal from '../components/Modal/Modal'
+import Backdrop from '../components/Backdrop/Backdrop'
 
-const events = () => {
-    return (<div>
-        <h1>Events </h1>
-        <button> Create Events</button>
-    </div>)
+class Events extends Component {
+
+    state = {
+        modalOpen: false
+    }
+
+  modalOpen = () => {
+      this.setState({
+        modalOpen: true
+      })  }
+
+      modalClose = () => {
+        this.setState({
+          modalOpen: false
+        })
+  };
+
+  
+
+    render () {
+        return (
+            <React.Fragment > 
+{this.state.modalOpen ? ( 
+              <React.Fragment >  
+              <Backdrop />
+             <Modal canCancel canConfirm closed={this.modalClose}> 
+            </Modal> </React.Fragment>) : null}
+      
+        <div className='events'>
+            <h1>Events </h1>
+            <button onClick = {this.modalOpen}> Create Events</button>
+        </div>
+        </React.Fragment>
+        )
+    }
+   
 }
 
-export default events
+export default Events
